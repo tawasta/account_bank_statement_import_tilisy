ab981cd9e2c5817f8a10a3d7e05e62a5b3ca4c2f
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2024-10-09 22:21:48 +0300

    Revert "[MIG] 17.0 account_bank_statement_import_tilisy"
    This reverts commit a7f3088ed72b408c8cf445c629a7b973ba5c506d.

--- wizards/online_bank_statement_pull_wizard.py
+++ wizards/online_bank_statement_pull_wizard.py
@@ -11,20 +11,20 @@ class OnlineBankStatementPullWizard(models.TransientModel):
 
     def action_pull(self):
         self.ensure_one()
-        provider = self._get_provider()
-        if provider.service == "tilisy":
-            # Check if authentication is still valid
-            tilisy = provider.tilisy_application_id
-            msg = _("You bank authentication is invalid. Please authenticate and try again")
-            if not tilisy.session:
-                raise ValidationError(msg)
-
-            session_dict = json.loads(tilisy.session)
-            valid_until = session_dict.get("access", {}).get("valid_until")
-            if valid_until:
-                datetime_format = "%Y-%m-%dT%H:%M:%S"
-                valid_datetime = datetime.strptime(valid_until[0:19], datetime_format)
-                if valid_datetime < fields.Datetime.now():
+        for provider in self.provider_ids:
+            if provider.service == "tilisy":
+                # Check if authentication is still valid
+                tilisy = provider.tilisy_application_id
+                msg = _("You bank authentication is invalid. Please authenticate and try again")
+                if not tilisy.session:
                     raise ValidationError(msg)
+
+                session_dict = json.loads(tilisy.session)
+                valid_until = session_dict.get("access", {}).get("valid_until")
+                if valid_until:
+                    datetime_format = "%Y-%m-%dT%H:%M:%S"
+                    valid_datetime = datetime.strptime(valid_until[0:19], datetime_format)
+                    if valid_datetime < fields.Datetime.now():
+                        raise ValidationError(msg)
 
         return super().action_pull()
